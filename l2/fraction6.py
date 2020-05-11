d47c78421c06abbeefdf7326275349c637ce1fe8
lelouch37 <roseaupensant01@gmail.com>
2020-05-11 20:02:57 +0900

    全体修正

--- l2/fraction6.py
+++ l2/fraction6.py
@@ -8,25 +8,19 @@ Created on Tue May  5 20:59:38 2020
 
 class fraction:
     def __init__(self, a, b):
-        self.__numerator = a
-        self.__denominator = b
-        self.reduction()
+        c = gcd(a, b)
+        self.__numerator = a // c
+        self.__denominator = b // c
         
     def disp(self):
         print(str(self.__numerator) + "/" + str(self.__denominator))
         
     def __add__(self, b):
-        c = fraction(self.__numerator*b.__denominator+self.__denominator
+        return fraction(self.__numerator*b.__denominator+self.__denominator
                         *b.__numerator, self.__denominator*b.__denominator)
-        return c
     
     def __repr__(self):
         return str(self.__numerator) + "/" + str(self.__denominator)
-    
-    def reduction(self):
-        tmp = gcd(self.__numerator, self.__denominator)
-        self.__numerator //= tmp
-        self.__denominator //= tmp
     
 def gcd(a, b):
     if b == 0:
